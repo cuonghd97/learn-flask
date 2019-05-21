b36522a6f8dbc691c509a7f702281fa77a3a0714
dangvv_58 <dang.vuvan@meditech.vn>
2019-05-21 17:31:04 +0700

    cuong

--- todo-mrf/frontend/src/ListFunctions.js
+++ todo-mrf/frontend/src/ListFunctions.js
@@ -8,13 +8,42 @@ export const get_list = () => {
             let todo_list = res.data.todo_list
             Object.keys(todo_list).forEach((key) => {
                 let val = todo_list[key]
-                data.push([val.id, val.work])
+                console.log(val)
+                data.push([val.id, val.work, val.is_done])
             })
+            console.log(data)
             return data
         })
 }
 
-export const add_to_list = () => {
+export const add_to_list = todo_item => {
     return axios
-    .get('')
+        .post(
+            '/api/todo',
+            { work: todo_item },
+            { headers: { "Content-type": "application/json" } }
+        )
+        .then(res => {
+            console.log(res)
+        })
+}
+
+export const delete_one = id => {
+    return axios
+        .delete(
+            `/api/todo/${id}`
+        )
+        .then(res => {
+            console.log(res)
+        })
+}
+
+export const check_item = id => {
+    return axios
+        .put(
+            `/api/todo/${id}`
+        )
+        .then(res => {
+            console.log(res)
+        })
 }
--- todo-mrf/frontend/src/components/List.js
+++ todo-mrf/frontend/src/components/List.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import classNames from 'classnames/bind'
+import { get_list, add_to_list, delete_one, check_item } from '../ListFunctions'
 
-import { get_list } from '../ListFunctions'
+import './List.css'
 
 class List extends Component {
     constructor() {
@@ -10,8 +12,12 @@ class List extends Component {
             id: '',
             work: '',
             isEdit: false,
-            todo_items: []
+            todo_items: [],
         }
+
+        this.onChange = this.onChange.bind(this)
+        this.removeItem = this.removeItem.bind(this)
+        this.checkItem = this.checkItem.bind(this)
     }
 
     componentDidMount() {
@@ -26,19 +32,73 @@ class List extends Component {
         })
     }
 
+    onChange = e => {
+        this.setState({
+            work: e.target.value
+        })
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        add_to_list(this.state.work).then(
+            () => {
+                this.getAll()
+            }
+        )
+    }
+
+    removeItem(item) {
+        return event => {
+            delete_one(item[0])
+                .then(() => {
+                    this.getAll()
+                })
+        }
+    }
+
+    checkItem(item) {
+        return event => {
+            check_item(item[0])
+                .then(() => {
+                    this.getAll()
+                })
+        }
+    }
+
     render() {
         const { todo_items } = this.state
+        console.log(todo_items)
         return(
             <div>
                 <div>
-                    <input type="text" name="work" id="work"/>
-                    <button>Add</button>
+                    <form>
+                        <input
+                            type="text"
+                            name="work"
+                            id="work"
+                            defaultValue={this.state.work}
+                            onChange={this.onChange}
+                        />
+                        <button onClick={this.onSubmit}>Add</button>
+                    </form>
                 </div>
+                <div className="todolist">
                 {
                     todo_items.map((item, index) => {
-                        return <p key={index}>{item[1]}</p>
+                        return (
+                            <p
+                                key={index}
+                                className={classNames({
+                                    'done': item[2] !== 0
+                            })}>
+                                <button onClick={this.checkItem(item)}>0</button>
+                                {item[1]}
+                                <button onClick={this.removeItem(item)}>remove</button>
+                            </p>
+                        )
                     })
                 }
+                </div>
             </div>
         )
     }
