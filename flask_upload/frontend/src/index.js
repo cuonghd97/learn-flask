aa2a542cde627c0f4292979e11ac67d16c716704
dangvv_58 <dang.vuvan@meditech.vn>
2019-05-22 18:32:46 +0700

    cuong

--- flask_upload/frontend/src/index.js
+++ flask_upload/frontend/src/index.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react"
 import ReactDOM from "react-dom"
 import axios from "axios"
-axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+import { notify } from "react-notify-toast"
+// axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
 class App extends Component {
-    login = () => {
+    constructor() {
+        super()
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+
+        this.changeValue = this.changeValue.bind(this)
+    }
+    login = (username, password) => {
         return axios
             .post(
                 "http://192.168.40.225:5000/login",
                 {
-                    "username": "cuong1",
-                    "password": "1"
+                    "username": username,
+                    "password": password
                 },
                 { headers: { "Content-type": "application/json" } }
             )
             .then(res => {
-                console.log(res)
+                console.log(res.data.access_token)
+                localStorage.setItem("access_token", res.data.access_token)
             })
     }
 
@@ -22,15 +34,42 @@ class App extends Component {
     }
 
     click = () => {
-        console.log("click")
-        this.login()
+        // notify.show("Hello")
+        // this.login()
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        this.login(this.state.username, this.state.password)
+    }
+
+    changeValue = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     render() {
+        console.log(this.state)
         return(
             <div>
                 <h1>Hello world</h1>
-                <button onClick={this.click}>Click</button>
+                <form method="post">
+                    <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder="username"
+                        onChange={this.changeValue}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={this.changeValue}
+                    />
+                    <button onClick={this.handleFormSubmit}>Login</button>
+                </form>
             </div>
         )
     }
